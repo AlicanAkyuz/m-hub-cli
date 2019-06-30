257432b149e24b2e22a35645ecbb4ff244e97904
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-06-30 09:36:30 +0300

    wakeup taken back

--- src/components/pages/Splash.js
+++ src/components/pages/Splash.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useContext } from "react";
 import { Redirect } from "react-router-dom";
 import jwt_decode from "jwt-decode";
-import axios from "axios";
-import root from "../../utils/root";
+
 import Context from "../../context";
 import Login from "../auth/Login";
 import AuthToken from "../../utils/AuthToken";
@@ -11,18 +10,6 @@ const Splash = () => {
   const { state, dispatch } = useContext(Context);
 
   useEffect(() => {
-    // wake up the server
-    const wakeServerUp = async () => {
-      try {
-        const res = await axios.get(`${root}/users/wakeup`);
-        if (res) console.log(res.data.msg);
-      } catch (err) {
-        console.log("Server is not responding!");
-      }
-    };
-
-    wakeServerUp();
-
     // if there is token in local storage, set token headers, user, and auth & user state
     if (localStorage.jwt_token) {
       AuthToken(localStorage.jwt_token);
